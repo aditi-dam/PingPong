5efeac236be1db7a6f2377e71d3e5ab084025e08
aaacoder <aditi.dam@gmail.com>
2020-05-27 09:24:26 -0400

    Aditi. 5/27. 9:24 am. Instructions

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
@@ -12,8 +14,22 @@ public class InstructionScreen extends Application{
         ps = primaryStage;
         Pane pane = new Pane(); 
 
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setHeaderText("How to Play:");
+        alert.setContentText(
+            "Welcome to Ping Pong!\n\nCan you keep the ball from bouncing offscreen "+
+            "for the entire time, or will this game get the best of you?\n\n\n"+
+            "After reading this page, click on the button to return to the "+
+            "welcome screen. Then you can click the play button to start the real game."+
+            "Use your paddle to keep the ball from getting past you as it bounces off "+
+            "the walls. Can you do it until time's up?\n"+
+            "If you succeed, you'll be a Ping Pong champion!"+
+            "Ready to start?");
+
+        alert.showAndWait();
+
         Scene scene = new Scene(pane); 
-        ps.setTitle("How to Play:");
+        ps.setTitle("Instructions");
         ps.setScene(scene); 
         ps.show();
 
