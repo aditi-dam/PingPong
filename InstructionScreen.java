3add2d873845b3ea8da5c5d2eee2bd314f0bf1b7
Satwika <satwika.vemuri@gmail.com>
2020-05-27 08:31:17 -0400

    Same thing again

---
+++ InstructionScreen.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.Scene;
+
+public class InstructionScreen extends Application{
+    Stage ps;
+    
+    @Override
+    public void start(Stage primaryStage){
+        ps = primaryStage;
+        Pane pane = new Pane(); 
+
+        Scene scene = new Scene(pane); 
+        ps.setTitle("How do you play?");
+        ps.setScene(scene); 
+        ps.show();
+
+    }
+
+}
--- WelcomeScreen.java
+++ WelcomeScreen.java
@@ -1,20 +1,24 @@
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
 
 public class WelcomeScreen extends Application{
     private Button next = new Button("Next");
+    private Button instructions = new Button("Instructions");
     Stage ps;
     
     @Override
     public void start(Stage primaryStage){
         ps = primaryStage;
-        Pane pane = new Pane(); 
+        GridPane pane = new GridPane(); 
 
-        pane.getChildren().add(next);
+        pane.add(next, 0, 0);
+        pane.add(instructions, 0, 1);
+
         next.setOnAction(e -> showNextScreen());
+        instructions.setOnAction(e -> showInstructionsScreen());
 
         Scene scene = new Scene(pane); 
         ps.setTitle("The Scene is a real Pane");
@@ -26,6 +30,12 @@ public class WelcomeScreen extends Application{
     public void showNextScreen(){
         ps.close();
         PingPong game = new PingPong();
+        game.start(ps);
+    }
+
+    public void showInstructionsScreen(){
+        ps.close();
+        InstructionScreen game = new InstructionScreen();
         game.start(ps);
     }
 }
