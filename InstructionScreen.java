8d042e1f1a9495e916d8ae018d0160df8c52f35f
aaacoder <aditi.dam@gmail.com>
2020-05-27 10:02:48 -0400

    Fixing merge conflict on Instructions screen. 5/27 10:02 am

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,38 +1,39 @@
-import javafx.application.Application;
+import java.util.Optional;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
-import javafx.scene.Scene;
-
-public class InstructionScreen extends Application{
+ 
+public class InstructionScreen{
     Stage ps;
-    
-    @Override
+  
     public void start(Stage primaryStage){
         ps = primaryStage;
-        Pane pane = new Pane();
         showInstructions();
-
-        Scene scene = new Scene(pane); 
-        ps.setTitle("Instructions");
-        ps.setScene(scene); 
-        ps.show();
     }
-
+ 
     public void showInstructions() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setHeaderText("How to Play:");
         alert.setContentText(
             "Welcome to Ping Pong!\n\nCan you keep the ball from bouncing offscreen "+
             "for the entire time, or will this game get the best of you?\n\n\n"+
-            "After reading this page, click the OK button to return to the "+
-            "welcome screen. Then you can click the play button to start the real game."+
-            "\nUse your paddle to keep the ball from getting past you as it bounces off "+
+            "After reading this page, click on the button to return to the "+
+            "welcome screen. Then you can click the play button to start the real game. "+
+            "Use your paddle to keep the ball from getting past you as it bounces off "+
             "the walls. Can you do it until time's up?\n"+
-            "If you succeed, you'll be a Ping Pong champion! "+
+            "If you succeed, you'll be a Ping Pong champion!"+
             "Ready to start?");
+ 
+        ButtonType backToWelcomeScreen = new ButtonType("Go back to Welcome Screen");
+  
+        alert.getButtonTypes().setAll(backToWelcomeScreen);
+  
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == backToWelcomeScreen){
+            WelcomeScreen game = new WelcomeScreen();
+            game.start(ps);
+        }
     }
-
+ 
 }
