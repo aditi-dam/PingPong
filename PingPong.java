0f48f9a76dc78b290ec91645537d91c86137c54a
Satwika <satwika.vemuri@gmail.com>
2020-05-28 14:31:32 -0400

    Paddle resizes with screen now

--- PingPong.java
+++ PingPong.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.Scene;
 
@@ -9,16 +10,17 @@ public class PingPong extends Application{
     private Stage ps;
     private Button win = new Button("Win");
     private Button lose = new Button("Lose");
+    private Rectangle paddle = new Rectangle(70, 35, Color.BLACK);
     
     @Override
     public void start(Stage primaryStage){
         ps = primaryStage;
-        ps.setWidth(350);
-        ps.setHeight(350);
         Pane pane = new Pane(); 
-
-        Paddle paddle = new Paddle(pane);
-        paddle.draw();
+        paddle.setX(pane.getWidth()/2);
+        paddle.setY(pane.getHeight() - 10);
+        paddle.xProperty().bind(pane.widthProperty().divide(2));
+        paddle.yProperty().bind(pane.heightProperty().subtract(10));
+        pane.getChildren().add(paddle);
 
         //pane.getChildren().addAll(win, lose);
 
