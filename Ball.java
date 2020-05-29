dca3e327526531c7352bd115dead71e82a20404f
aaacoder <aditi.dam@gmail.com>
2020-05-29 10:29:23 -0400

    I fix Ball

--- Ball.java
+++ Ball.java
@@ -1,12 +1,8 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.beans.property.DoubleProperty;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Ball extends Pane{
