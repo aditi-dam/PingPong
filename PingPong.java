a3d8b66db6b5e0d3d082ceda7406c41722b88fff
Satwika <satwika.vemuri@gmail.com>
2020-05-29 10:02:26 -0400

    Paddle moves smoothly, no random print statements

--- PingPong.java
+++ PingPong.java
@@ -11,8 +11,7 @@ public class PingPong extends Application{
     private Stage ps;
     private Button win = new Button("Win");
     private Button lose = new Button("Lose");
-    private int paddleX = 70;
-    private Rectangle paddle = new Rectangle(paddleX, 35, Color.BLACK);
+    private Rectangle paddle = new Rectangle(70, 35, Color.BLACK);
     private Pane pane = new Pane(); 
     
     @Override
@@ -21,22 +20,17 @@ public class PingPong extends Application{
         Scene scene = new Scene(pane, 200, 200); 
         paddle.setX(pane.getWidth()/2);
         paddle.setY(pane.getHeight() - 10);
-        //paddle.xProperty().bind(pane.widthProperty().divide(2));
         paddle.yProperty().bind(pane.heightProperty().subtract(10));
         pane.getChildren().add(paddle);
 
         scene.setOnKeyPressed(e -> {
             if(e.getCode() == KeyCode.RIGHT){
-                System.out.println("Right");
                 move(10);
             }
             else if(e.getCode() == KeyCode.LEFT){
-                System.out.println("Left");
                 move(-10);
             }
         });
-
-        //pane.getChildren().addAll(win, lose);
 
         win.setOnAction(e -> win());
         lose.setOnAction(e -> lose());
@@ -60,10 +54,7 @@ public class PingPong extends Application{
     }
 
     public void move(double x){
-        System.out.println("At move");
-        System.out.println(paddle.getX());
-        paddle.setX(paddle.getX() + x); //setX() not working for some reason
-        System.out.println("Set x");
+        paddle.setX(paddle.getX() + x); 
         if(paddle.getX() < 0){
             paddle.setX(0);
        }
