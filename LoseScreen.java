d721e01cdf641238e0a56e1ad9298d6ff6de1fe3
aaacoder <aditi.dam@gmail.com>
2020-06-02 11:02:03 -0400

    lose screen changes

--- LoseScreen.java
+++ LoseScreen.java
@@ -1,5 +1,7 @@
 import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class LoseScreen{
@@ -11,8 +13,16 @@ public class LoseScreen{
     public void showLoseScreen(){
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("LOSER!!!!!!");
-        alert.setHeaderText("Look, an Error Dialog");
-        alert.setContentText("Ooops, there was an error!");
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        
+        alert.setHeaderText("You lost.");
+        alert.setContentText("Looks like you failed to hit the ball."+
+        "And it wasn't a minute yet."+"Better luck next time!");
+
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+            getClass().getResource("instructions.css").toExternalForm());
+        dialogPane.getStyleClass().add(".dialog-pane2");
 
         alert.showAndWait();
     }
